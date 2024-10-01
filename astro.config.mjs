8e264ae45756b2008954575a3195a81b11560f19
Jose Hernandez <josemanki@gmail.com>
2024-10-01 10:18:49 +0200

    builds page server-side, mobile navbar implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
-    domains: ['dofusdu.de'],
+    domains: ['dofusdu.de']
   },
+  output: "server",
+  adapter: netlify()
 });
